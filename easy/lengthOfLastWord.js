ce6d05d550e2e78b0ce2e847b10386ca3769cd3a
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-07-26 16:07:40 -0700

    Length of last Word

---
+++ easy/lengthOfLastWord.js
@@ -0,0 +1,66 @@
+/*
+
+58. Length of Last Word
+Easy
+
+1428
+
+96
+
+Add to List
+
+Share
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+Accepted
+810,097
+Submissions
+2,077,240
+Seen this question in a real interview before?
+
+Yes
+
+No
+
+Problems
+
+Pick One
+
+
+
+
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+
+};
